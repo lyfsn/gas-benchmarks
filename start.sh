c6646c5735b18b27c4563292a5a5443ec3f4f96b
smallyunet <dev.wangyu@gmail.com>
2024-06-03 14:54:04 +0800

    up

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
-bash ./run.sh -t "tests/" -w "warmup/warmup-1000bl-16wi-24tx.txt" -c "nethermind,geth,reth,erigon,besu" -r 8
+docker stop gas-execution-client
+docker stop gas-execution-client-sync
+
+docker rm gas-execution-client
+docker rm gas-execution-client-sync
+
+nohup ./run.sh -t "tests/" -w "warmup/warmup-1000bl-16wi-24tx.txt" -c "nethermind,geth,reth,erigon,besu" -r 8
 nohup &
 
 
