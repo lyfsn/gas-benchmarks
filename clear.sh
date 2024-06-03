a17497c22775ddfb138b1d12c452fa2ca74ca88d
smallyunet <dev.wangyu@gmail.com>
2024-06-03 14:55:07 +0800

    up

--- clear.sh
+++ clear.sh
@@ -1,2 +1,13 @@
 rm -rvf reports
 rm -rvf results
+
+rm nohup.out
+
+
+docker stop gas-execution-client
+docker stop gas-execution-client-sync
+
+docker rm gas-execution-client
+docker rm gas-execution-client-sync
+
+
--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
-docker stop gas-execution-client
-docker stop gas-execution-client-sync
 
-docker rm gas-execution-client
-docker rm gas-execution-client-sync
+
 
 nohup ./run.sh -t "tests/" -w "warmup/warmup-1000bl-16wi-24tx.txt" -c "nethermind,geth,reth,erigon,besu" -r 8
 nohup &
