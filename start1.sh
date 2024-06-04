65d1af131829e0db799a322f2222a8646505d9dd
lyfsn <dev.wangyu@proton.me>
2024-06-04 10:55:01 +0800

    addoutput

--- start1.sh
+++ start1.sh
@@ -5,10 +5,11 @@
 
 nohup \
   ./run.sh \
-  -t "tests/" \
+  -t "tests1/" \
   -w "warmup/warmup-1000bl-16wi-24tx.txt" \
   -c "nethermind,geth,reth,erigon,besu" \
   -r 8 \
+  -o "results1" \
   > output.log 2>&1 &
 
 
