66ea5030f0450d00f9c52384bf49856e03d7d26f
lyfsn <dev.wangyu@proton.me>
2024-06-04 14:00:45 +0800

    up

---
+++ start1.sh
@@ -0,0 +1,44 @@
+
+
+
+#!/bin/bash
+
+./run.sh \
+  -t "tests1/" \
+  -w "warmup/warmup-1000bl-16wi-24tx.txt" \
+  -c "nethermind,geth,reth,erigon,besu" \
+  -r 8 \
+  -o "results1"
+
+
+./run.sh \
+  -t "tests1/" \
+  -w "warmup/warmup-1000bl-16wi-24tx.txt" \
+  -c "nethermind,geth,reth,erigon,besu" \
+  -r 8 \
+  -o "results2"
+
+./run.sh \
+  -t "tests1/" \
+  -w "warmup/warmup-1000bl-16wi-24tx.txt" \
+  -c "nethermind,geth,reth,erigon,besu" \
+  -r 8 \
+  -o "results3"
+
+
+
+
+python3 report_html.py \
+  --clients "nethermind,geth,reth,erigon,besu" \
+  --testsPath "tests1" \
+  --resultsPath "results1"
+
+python3 report_html.py \
+  --clients "nethermind,geth,reth,erigon,besu" \
+  --testsPath "tests1" \
+  --resultsPath "results2"
+
+python3 report_html.py \
+  --clients "nethermind,geth,reth,erigon,besu" \
+  --testsPath "tests1" \
+  --resultsPath "results3"
