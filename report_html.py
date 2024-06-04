1d6238b86b6a0d0e930a049b13ff6b65d8fbcbb6
lyfsn <dev.wangyu@proton.me>
2024-06-04 11:22:46 +0800

    test for report

--- report_html.py
+++ report_html.py
@@ -136,7 +136,7 @@ def get_html_report(client_results, clients, results_paths, test_cases, methods,
 
     soup = BeautifulSoup(results_to_print, 'lxml')
     formatted_html = soup.prettify()
-    print(formatted_html)
+    # print(formatted_html)
     if not os.path.exists('reports'):
         os.mkdir('reports')
     with open(f'reports/index.html', 'w') as file:
@@ -217,6 +217,7 @@ def main():
         for item in data:
             metadata[item['Name']] = item
 
+    print("client_results" + str(client_results))
     get_html_report(client_results, clients.split(','), results_paths, test_cases, methods, gas_set, metadata)
 
     print('Done!')
