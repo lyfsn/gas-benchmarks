706541783652bcf6f57a8a10c8fe0d57d26b0fcd
lyfsn <dev.wangyu@proton.me>
2024-06-05 17:08:20 +0800

    up

--- start4.sh
+++ start4.sh
@@ -42,3 +42,7 @@ python3 report_html.py \
   --testsPath "tests3" \
   --resultsPath "results3_1" \
   --runs 1
+
+./nethermind/tools/Nethermind.Tools.Kute/bin/Release/net8.0/Nethermind.Tools.Kute -i tests3/MyTest/MyTest_1M.txt -s /tmp/jwtsecret -r results3_1/nethermind_response_1_MyTest_1M.txt -a http://localhost:8551 
+./nethermind/tools/Nethermind.Tools.Kute/bin/Release/net8.0/Nethermind.Tools.Kute -i tests3/MyTest/MyTest_1M.txt -s ./jwtsecret -r results3_1/nethermind_response_1_MyTest_1M.txt -a http://localhost:8551 
+./nethermind/tools/Nethermind.Tools.Kute/bin/Release/net8.0/Nethermind.Tools.Kute -i tests3/MyTest/test.txt -s ./jwtsecret -r results3_1/nethermind_response_1_MyTest_1M.txt -a http://localhost:8551 
