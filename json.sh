5aeb759870646179fb465a13fb06b88de89cbaeb
lyfsn <dev.wangyu@proton.me>
2024-06-06 15:44:11 +0800

    up

--- json.sh
+++ json.sh
@@ -7,11 +7,3 @@ python3 report_json.py \
   --resultsPath "results" \
   --runs 8
 
-
-
-python3 report_json.py \
-  --clients "nethermind,geth,reth,erigon,besu" \
-  --testsPath "tests3" \
-  --resultsPath "results3_1" \
-  --runs 4
-
