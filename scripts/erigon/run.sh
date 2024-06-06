c52230cab573708588bc12d449116de72f8f8630
lyfsn <dev.wangyu@proton.me>
2024-06-06 17:31:41 +0800

    up

--- scripts/erigon/run.sh
+++ scripts/erigon/run.sh
@@ -1,6 +1,6 @@
 # Prepare geth image that we will use on the script
 cd scripts/erigon
-cp ../el-cl-genesis-data/custom_config_data/genesis.json /tmp/genesis.json
+cp ../../../el-cl-genesis-data/custom_config_data/genesis.json /tmp/genesis.json
 cp jwtsecret /tmp/jwtsecret
 
 docker compose up -d
--- scripts/geth/run.sh
+++ scripts/geth/run.sh
@@ -1,7 +1,7 @@
 # Prepare geth image that we will use on the script
 cd scripts/geth
 pwd
-cp ../el-cl-genesis-data/custom_config_data/genesis.json /tmp/genesis.json
+cp ../../../el-cl-genesis-data/custom_config_data/genesis.json /tmp/genesis.json
 cp jwtsecret /tmp/jwtsecret
 
 docker compose up -d
