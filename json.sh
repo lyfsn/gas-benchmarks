11180a8d59d30607e00feda384c0aad37be2b7f5
lyfsn <dev.wangyu@proton.me>
2024-06-06 15:39:00 +0800

    test

---
+++ json.sh
@@ -0,0 +1,14 @@
+
+
+
+python3 report_json.py \
+  --clients "nethermind,geth,reth,erigon,besu" \
+  --testsPath "tests" \
+  --resultsPath "results"
+
+
+
+python3 report_json.py \
+  --clients "nethermind,geth,reth,erigon,besu" \
+  --testsPath "tests3" \
+  --resultsPath "results3_1"
