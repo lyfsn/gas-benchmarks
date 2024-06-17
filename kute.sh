f0a40e9aff148d1e97c9b4d7d49232f880d893b8
lyfsn <dev.wangyu@proton.me>
2024-06-17 11:42:16 +0800

    up

--- kute.sh
+++ kute.sh
@@ -2,7 +2,7 @@
 mkdir results
 
 ./nethermind/tools/Nethermind.Tools.Kute/bin/Release/net8.0/Nethermind.Tools.Kute \
-  -i testburnt/Burnt/Burnt_90M.txt \
+  -i testburntwarm/warm.txt \
   -s scripts/nethermind/jwtsecret \
   -r results/res.txt \
   -a http://127.0.0.1:8551
