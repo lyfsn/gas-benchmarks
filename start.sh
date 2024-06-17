ee792635e89d962bddca791f2a4ca2590fafb2d2
lyfsn <dev.wangyu@proton.me>
2024-06-17 11:43:29 +0800

    up

--- start.sh
+++ start.sh
@@ -5,35 +5,8 @@
 
 nohup \
   ./run.sh \
-  -t "tests/" \
-  -w "warmup/warmup-1000bl-16wi-24tx.txt" \
-  -c "nethermind,geth,reth,erigon,besu" \
-  -r 8 \
-  -o "results" \
-  > output.log 2>&1 &
-
-
-
-./run.sh \
   -t "testburnt/" \
-  -w "./testburnt/Burnt/Burnt_90M.txt" \
-  -c "nethermind" \
-  -r 1 \
-  -o "results"
-
-
-./run.sh \
-  -t "testburnt/" \
-  -w "./testburnt/Burnt/Burnt_90M.txt" \
-  -c "nethermind" \
-  -r 1 \
-  -o "results"
-
-
-nohup \
-  ./run.sh \
-  -t "testburnt/" \
-  -w "./testburnt/Burnt/Burnt_90M.txt" \
+  -w "./testburntwarm/warm.txt" \
   -c "nethermind,erigon,geth,reth,besu" \
   -r 8 \
   -o "results" \
