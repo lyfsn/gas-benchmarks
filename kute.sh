d51c874e6b08f2e7c900ab687f26258cd6996218
lyfsn <dev.wangyu@proton.me>
2024-06-28 17:03:46 +0800

    init and merge devnet code

--- kute.sh
+++ kute.sh
@@ -1,9 +1,9 @@
 
-
+mkdir results
 
 ./nethermind/tools/Nethermind.Tools.Kute/bin/Release/net8.0/Nethermind.Tools.Kute \
-  -i tests1/Address/Address_30M.txt \
-  -s scripts/geth/jwtsecret \
-  -r results_1/1.txt \
+  -i testburntwarm/warm.txt \
+  -s scripts/nethermind/jwtsecret \
+  -r results/res.txt \
   -a http://127.0.0.1:8551
 
--- start.sh
+++ start.sh
@@ -5,10 +5,9 @@
 
 nohup \
   ./run.sh \
-  -t "tests/" \
-  -w "warmup/warmup-1000bl-16wi-24tx.txt" \
-  -c "nethermind,geth,reth,erigon,besu" \
+  -t "testburnt/" \
+  -w "./testburntwarm/warm.txt" \
+  -c "nethermind,erigon,geth,reth,besu" \
   -r 8 \
   -o "results" \
   > output.log 2>&1 &
-
