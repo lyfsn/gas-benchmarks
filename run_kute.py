01ec8ca9a478d36bfc4cb78497dd6cec7c8d4b91
lyfsn <dev.wangyu@proton.me>
2024-08-09 12:50:19 +0800

    up

--- run_kute.py
+++ run_kute.py
@@ -107,11 +107,12 @@ def main():
                     response)
         return
     else:
-        response_file = os.path.join(output_folder, f'{client}_response_{run}_Burnt.txt')
+        name = test_case_path.split('/')[-1].split('.')[0]
+        response_file = os.path.join(output_folder, f'{client}_response_{run}_{name}_Burnt.txt')
         print(f"Running {client} for the {run} time with test case {tests_paths}")
         response = run_command(tests_paths, jwt_path, response_file, execution_url, kute_arguments)
         test_case_without_extension = os.path.splitext(tests_paths.split('/')[-1])[0]
-        save_to(output_folder, f'{client}_results_{run}_Burnt.txt',
+        save_to(output_folder, f'{client}_results_{run}_{name}_Burnt.txt',
                 response)
 
 
